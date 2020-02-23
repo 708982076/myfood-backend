e1378b59105edaaeb35461f0a993251ee2890b26
chengpeng <chengpeng_1231@163.com>
2020-02-23 20:05:19 +0800

    123

--- models/cities/cities.js
+++ models/cities/cities.js
@@ -2,13 +2,20 @@ const mongoose = require('mongoose');
 const data = require('./cities-init.js')
 
 const CitiesSchema = new mongoose.Schema({
-  allCity: Object
+  allCity: Object,
+  hotCity: Array
 }, {versionKey: false});
 
 const Cities = mongoose.model('citie', CitiesSchema);
 
+let hotCity = ["北京", '上海', '广州', '深圳', '武汉', '杭州', '成都'];
+for (let i in data) {
+  hotCity.push( ...data[i].filter( city => hotCity.includes(city.name) ) );
+}
+
 Cities.findOne(null, (err, res) => {
-  if(!err && !res) new Cities({allCity: data}).save()
+  if(!err && !res) new Cities({allCity: data, hotCity: hotCity.slice(7)}).save()
 })
+
 
 module.exports = Cities;
